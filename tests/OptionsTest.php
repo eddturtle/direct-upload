ffa5ae367fac120bbc3906d9962b8e668600ed49
Edd Turtle <edd@designedbyaturtle.co.uk>
2021-04-24 08:29:38 +0100

    Fix tests

--- tests/OptionsTest.php
+++ tests/OptionsTest.php
@@ -10,12 +10,13 @@ class OptionsTest extends TestCase
     public function testGetOptions()
     {
         $options = (new Options())->getOptions();
-        $this->assertTrue(count($options) === 11);
+        $this->assertTrue(count($options) === 12);
         $this->assertArrayHasKey('success_status', $options);
         $this->assertArrayHasKey('acl', $options);
         $this->assertArrayHasKey('default_filename', $options);
         $this->assertArrayHasKey('max_file_size', $options);
         $this->assertArrayHasKey('expires', $options);
         $this->assertArrayHasKey('valid_prefix', $options);
+        $this->assertArrayHasKey('content_type', $options);
     }
 }
